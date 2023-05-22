da8f4baba87ae9eb09a54084608259f73df7ddc9
TAAbishek <112066367+TAAbishek@users.noreply.github.com>
2023-05-22 09:43:47 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const sub_button = document.getElementById("submitBtn");
+sub_button.addEventListener('click', myfunc);
+
+function myfunc() {
+    window.location.href = 'form.html';
+}
---
+++ javascripter.js
@@ -0,0 +1,5 @@
+const back_button=document.getElementById("backBtn")
+back_button.addEventListener('click',myfunc);
+function myfunc(){
+    window.location.href='index.html';
+}
